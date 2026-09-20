b6eaf4f6ad7700c7c485825669883f41fb0f2bd5
BlueRiver01 <316824754+BlueRiver01@users.noreply.github.com>
2026-09-19 17:21:03 -0700

    Fix dateFormat type to DateTimeFormatter

--- src/en/erosscans/src/eu/kanade/tachiyomi/extension/en/erosscans/ScytheScans.kt
+++ src/en/erosscans/src/eu/kanade/tachiyomi/extension/en/erosscans/ScytheScans.kt
@@ -1,17 +1,16 @@
-package eu.kanade.tachiyomi.extension.en.scythescans
+package eu.kanade.tachiyomi.extension.en.erosscans
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 import keiyoushi.annotation.Source
 import keiyoushi.network.rateLimit
 import okhttp3.OkHttpClient
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 @Source
 abstract class ScytheScans : MangaThemesia() {
 
-    // Matches "September 14, 2026" while safely supporting single-digit day formats
-    override val dateFormat = SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH)
+    override val dateFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("MMMM d, yyyy", Locale.ENGLISH)
 
     override fun OkHttpClient.Builder.configureClient() = rateLimit(3)
 }
